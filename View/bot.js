446ef50c0537106d6e93abfe4c128be674bd04e1
drakumus <rohancheeniyil@gmail.com>
2019-11-16 09:54:50 -0700

    restore old injection check

--- View/bot.js
+++ View/bot.js
@@ -20,8 +20,8 @@ const ach_hook = new Discord.WebhookClient(secret.ach_hook_id, secret.ach_hook_t
 const client = new Discord.Client();
 
 function checkForInjection(message) {
-	return  !(validator.isAlphanumeric(message.replace(/\s/g, '')));
-	//message.indexOf(';') == -1 ? false : true;
+	//return  !(validator.isAlphanumeric(message.replace(/\s/g, '')));
+	return message.indexOf(';') == -1 ? false : true;
 }
 
 function sleep(ms){
