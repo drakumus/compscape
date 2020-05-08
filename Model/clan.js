031b5389d684d792c4afb8bfb68750717cf772e6
drakumus <rohancheeniyil@gmail.com>
2020-05-08 07:16:48 -0600

    fix event end and resize arch icon for skill announcements

--- Model/clan.js
+++ Model/clan.js
@@ -490,8 +490,9 @@ async function getFormattedTopSkillExp(skill, timedTable = 'experience', numTop 
  */
 async function calculateTopExp(clan, timeSlot, numTop, catagory = "all", excludeInvention = false, isActiveEvent = false) {
     // find and store the difference between the timed table and the current exp table
-    var current_data = timeSlot === "event" ?  // && !isActiveEvent
-                       await  db.getClanData(clan, "end"):
+    // TODO: Write isActiveEvent to a settings file! Don't pipe it down.
+    var current_data = timeSlot === "event"// ?  // && !isActiveEvent
+                       // await  db.getClanData(clan, "end"):
                        await db.getClanData(clan);
     var timed_data = await db.getClanData(clan, timeSlot);
     var memberTotals = {}; // where the fuck did the rest go?
